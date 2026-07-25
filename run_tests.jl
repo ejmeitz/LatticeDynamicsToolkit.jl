f0809d0f93e475afeefdaaadfdd81f3deba59f86
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2026-07-25 17:08:58 -0400

    Delete run_tests.jl

--- run_tests.jl
+++
@@ -1,6 +0,0 @@
-# Convenience script to run tests: julia run_tests.jl
-# Alternative: julia -e 'using Pkg; Pkg.activate("."); include("test/runtests.jl")'
-using Pkg
-Pkg.activate(@__DIR__)
-Pkg.instantiate()
-include(joinpath(@__DIR__, "test", "runtests.jl"))
